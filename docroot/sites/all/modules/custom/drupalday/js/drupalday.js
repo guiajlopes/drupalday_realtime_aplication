7c57711c61537bb5ba5d85b0658d34d1206083ff
guilopes <guiajlopes@gmail.com>
2014-12-03 14:06:31 -0200

    Retirando console.log e corrigindo comentario no arquivo js

--- docroot/sites/all/modules/custom/drupalday/js/drupalday.js
+++ docroot/sites/all/modules/custom/drupalday/js/drupalday.js
@@ -1,12 +1,10 @@
 (function ($) {
   /**
-   * Drupal.Nodejs.callback on views update.
+   * Drupal.Nodejs.callback on drupalday.
    */
   Drupal.Nodejs.callbacks.drupalday = {
     callback: function (message) {
       if (message.channel == 'drupalday_form') {
-        console.log(message);
-        console.log(message.data.append_content);
         $('#mensages-drupalday-form').append('<p>' + message.data.append_content + '</p>');
       }
     }
